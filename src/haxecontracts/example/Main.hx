4d3cda00c47045de9a89ce57e7d9617f5bce8158
Andreas <ciscoheat@gmail.com>
2014-01-30 02:43:04 +0100

    Text fixes in Main.hx

--- src/haxecontracts/example/Main.hx
+++ src/haxecontracts/example/Main.hx
@@ -5,16 +5,15 @@ class Main
 	static function main() 
 	{
 		Sys.println("");
-		Sys.println("Press a key to create a Rational number 12/6.");
+		Sys.println("Press a key to create the Rational number 12/6.");
 		Sys.getChar(false);
 		
 		var r = new Rational(12, 6);
 		trace(r.toFloat());
 		
-		Sys.println("Press a key to create a Rational where the enumerator is zero.");
+		Sys.println("Press a key to create a Rational number where the denominator is zero.");
 		Sys.getChar(false);
 		
-		var r2 = new Rational(12, 0);
-		trace(r2.toFloat());
+		new Rational(12, 0);
 	}	
 }
