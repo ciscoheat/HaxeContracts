83135a9423854556d6f394277ace0fdd917b9586
Andreas <ciscoheat@gmail.com>
2015-01-13 23:21:49 +0700

    Updated example a little.

--- src/haxecontracts/example/Main.hx
+++ src/haxecontracts/example/Main.hx
@@ -1,21 +1,25 @@
 package haxecontracts.example;
 import haxecontracts.Contract;
 
-class Main 
+class Main
 {
-	static function main() 
+	static function main()
 	{
 		Sys.println("");
 		Sys.println("Press a key to create the Rational number 12/6.");
 		Sys.getChar(false);
-		
+
 		var r = new Rational(12, 6);
 		trace(r.toFloat() + " (" + r + ")");
-		
+
+		Sys.println("Setting the denominator to 3.");
+		r.denominator = 3;
+		trace(r.toFloat() + " (" + r + ")");
+
 		Sys.println("Press a key to create a Rational number where the denominator is zero.");
 		Sys.getChar(false);
-		
+
 		var r2 = new Rational(12, 0);
 		trace(r2.toFloat());
-	}	
+	}
 }
--- src/haxecontracts/example/Rational.hx
+++ src/haxecontracts/example/Rational.hx
@@ -4,33 +4,33 @@ import haxecontracts.Contract;
 
 class Rational implements HaxeContracts
 {
-	public var numerator(default, default) : Int;	
+	public var numerator(default, default) : Int;
     public var denominator(default, set) : Int;
 
-    public function new(numerator : Int, denominator : Int) 
+    public function new(numerator : Int, denominator : Int)
 	{
         this.numerator = numerator;
         this.denominator = denominator;
     }
-	
+
 	public function toFloat() : Float
 	{
 		return numerator / denominator;
 	}
-	
+
 	public function toString() : String
 	{
 		return numerator + "/" + denominator;
 	}
-	
-	private function set_denominator(v : Int) 
+
+	private function set_denominator(d : Int)
 	{
 		Contract.ensures(Contract.result != 0, "Denominator cannot be zero");
-        return denominator = v;
+        return denominator = d;
     }
 
     // Invariants will we injected at the end of every public method and in accessor methods.
-    @invariant private function objectInvariant() 
+    @invariant private function objectInvariant()
 	{
         Contract.invariant(denominator != 0, "Denominator cannot be zero");
     }
